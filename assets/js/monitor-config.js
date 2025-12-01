30ae543853e766e9ce130b6e906cd22eb15c35c8
ImTheBus <dean@imthebus.co.uk>
2025-12-01 11:44:43 +0000

    Create monitor-config.js

---
+++ assets/js/monitor-config.js
@@ -0,0 +1,9 @@
+// /monitor/assets/js/monitor-config.js
+// Phase 1 stub - real endpoints and intervals will come later
+export const monitorConfig = {
+  themes: 4,
+  pollIntervals: {
+    cycleCounterMs: 1800,
+    fakeLogMs: 5000
+  }
+};
